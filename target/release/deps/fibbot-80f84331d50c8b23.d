2cdcb45b70baaf8a279d6a099498170a24546136
dericko681 <abonghoderick@gmail.com>
2025-02-26 08:04:00 +0100

    incudes logic for extraction and fibo calculation

---
+++ target/release/deps/fibbot-80f84331d50c8b23.d
@@ -0,0 +1,5 @@
+/home/kunga-derick/projects/rust_programming/fibot/target/release/deps/fibbot-80f84331d50c8b23: src/main.rs
+
+/home/kunga-derick/projects/rust_programming/fibot/target/release/deps/fibbot-80f84331d50c8b23.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/fibbot.d
@@ -0,0 +1,1 @@
+/home/kunga-derick/projects/rust_programming/fibot/target/release/fibbot: /home/kunga-derick/projects/rust_programming/fibot/src/main.rs
