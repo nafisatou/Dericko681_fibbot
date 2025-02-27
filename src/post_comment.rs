a2e51f361cae024c8b914669da63253a28f25919
dericko681 <abonghoderick@gmail.com>
2025-02-27 16:34:12 +0100

    added comments to pull request

--- src/post_comment.rs
+++ src/post_comment.rs
@@ -1,9 +1,17 @@
 use reqwest::blocking::Client;
 use std::env;
 
-pub fn post_comment(pr_number: u32, owner: &str, repo: &str, comment: String) -> Result<(), Box<dyn std::error::Error>> {
+pub fn post_comment(
+    pr_number: u32,
+    owner: &str,
+    repo: &str,
+    comment: String,
+) -> Result<(), Box<dyn std::error::Error>> {
     let token = env::var("GITHUB_TOKEN")?;
-    let url = format!("https://api.github.com/repos/{}/{}/pulls/{}/comments", owner, repo, pr_number);
+    let url = format!(
+        "https://api.github.com/repos/{}/{}/pulls/{}/comments",
+        owner, repo, pr_number
+    );
 
     let client = Client::new();
     let response = client
@@ -12,10 +20,10 @@ pub fn post_comment(pr_number: u32, owner: &str, repo: &str, comment: String) ->
         .bearer_auth(token)
         .json(&serde_json::json!({ "body": comment }))
         .send()?;
-
+    println!("The URL is :", url);
     if response.status().is_success() {
         Ok(())
     } else {
-        Err("Failed to post comment".into())
+        Err(format!("Failed to post comment: {} - {}", response.status(), body).into())
     }
 }
