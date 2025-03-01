1e05999580eb447a39aa3edb3150b92b60856555
nafisatou <nafisatouhamadou2@gmail.com>
2025-03-01 16:24:58 +0100

    Update extract.rs

--- src/extract.rs
+++ src/extract.rs
@@ -11,44 +11,4 @@ pub fn extract_numbers(content: String) -> Vec<u32> {
         })
         .collect()
 }
-// pub fn extract_numbers(content: String) -> Vec<u32> {
-//     content
-//         .replace("-", " ")
-//         .split_whitespace()
-//         .map(|word| word.chars().filter(|c| !c.is_ascii_punctuation()).collect::<String>())
-//         .filter_map(|word| word.parse().ok())
-//         .collect()
-// }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_extract_numbers_for_special_chars() {
-//         let content = "Here are some numbers: 1; 42 and 100.".to_string();
-//         let numbers = extract_numbers(content);
-//         assert_eq!(numbers, vec![1, 42, 100]);
-//     }
-
-//     #[test]
-//     fn test_extract_numbers_for_letters() {
-//         let content = "Here are some numbers: 1b, 42D and E100F.".to_string();
-//         let numbers = extract_numbers(content);
-//         assert_eq!(numbers, vec![1, 42, 100]);
-//     }
-
-//     #[test]
-//     fn test_extract_numbers_for_letters_special() {
-//         let content = "Here are some numbers: 1b; w2d32-23?+ ++j12 42D and E100F.".to_string();
-//         let numbers = extract_numbers(content);
-//         assert_eq!(numbers, vec![1, 23223, 12, 42, 100]);
-//     }
-
-//     #[test]
-//     fn test_extract_numbers_for_no_number() {
-//         let content = "Here are some numbers: hello there".to_string();
-//         let numbers = extract_numbers(content);
-//         assert!(numbers.is_empty());
-//     }
-// }
